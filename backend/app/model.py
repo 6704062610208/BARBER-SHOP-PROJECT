7ce1cb6d093b3a87b63639d6e54b02e881a1e3c0
Akkadecht <jayjamesnew4@gmail.com>
2026-02-24 20:50:47 +0700

    Edit Barber_db add deualt is_active in users

--- backend/app/model.py
+++ backend/app/model.py
@@ -57,7 +57,7 @@ class User(Base):
     profile_img:Mapped[str | None] = mapped_column(String(255))#ใส่path
     create_at:Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
     update_at:Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
-    is_active:Mapped[bool] = mapped_column(Boolean,nullable=False)
+    is_active:Mapped[bool] = mapped_column(Boolean,nullable=False,default=False)
     queues:Mapped[list["QueueSlots"]] = relationship(back_populates="customer")
 
 working_slot_table = Table(
